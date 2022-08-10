5f13bda3ccaaa3d2777b5d9012ad646adb7872ed
Peng <peng.wei@murobotix.com>
2022-08-10 16:25:29 -0700

    add buffer and msg reading

--- src/voc-sampler/scripts/sampler_commander.py
+++ src/voc-sampler/scripts/sampler_commander.py
@@ -18,11 +18,18 @@ class SerialCommander(object):
 
     def init_variable(self):
         self.is_active = False
+        self.sample = {
+            'date': None,
+            'time': None,
+            'humidity': None,
+            'temperature': None
+
+        }
 
     def get_ros_parameter(self):
         port = rospy.get_param("~port", "/dev/ttyACM0")
         baud_rate = rospy.get_param("~baud_rate", 115200)
-        self.serial = serial.Serial(port, baud_rate)
+        self.serial = serial.Serial(port, baud_rate, timeout=1.0)
 
     def define_service(self):
         rospy.Service("command_sampler", CommandSampler, self.command_sampler)
@@ -31,16 +38,18 @@ class SerialCommander(object):
     def command_sampler(self, req):
         # s: Recording Start
         # r: Recording Stop
-       
         if req.command == CommandSampler._request_class.COMMAND_MOTOR_START:
-            self.serial.write(bytes(req.command, 'UTF-8'))
-            self.is_active = True       
-            rospy.loginfo("[voc_sampler]: Motor Started!")
+            if not self.is_active:
+                self.serial.reset_input_buffer()
+                self.serial.write(bytes(req.command, 'UTF-8'))
+                self.is_active = True       
+                rospy.loginfo("[voc_sampler]: Motor Started!")
 
         elif req.command == CommandSampler._request_class.COMMAND_MOTOR_STOP:
-            self.serial.write(bytes(req.command, 'UTF-8'))
-            self.is_active = False
-            rospy.loginfo("[voc_sampler]: Motor Stoped!")
+            if self.is_active:
+                self.serial.write(bytes(req.command, 'UTF-8'))
+                self.is_active = False
+                rospy.loginfo("[voc_sampler]: Motor Stoped!")
         
         else:
             rospy.logwarn("[voc_sampler]: Unrecognized command to sampler!")
@@ -51,13 +60,32 @@ class SerialCommander(object):
         while not rospy.is_shutdown():
             if self.is_active:
                 msg = self.serial.readline().decode("utf-8")
-                print(msg)
-            
+                msg_list = msg.split(',')
+                if len(msg_list) > 1:
+                    self.sample['date'] = msg_list[0]
+                    self.sample['time'] = msg_list[1]
+                    self.sample['humidity'] = msg_list[2]
+                    self.sample['temperature'] = msg_list[3]
+                    for key in self.sample:
+                        print(key + ": " + self.sample[key])
+                
             self.rate.sleep()
+
+    def clean(self):
+        # stop sampler and serial when exit
+        self.serial.write(
+            bytes(CommandSampler._request_class.COMMAND_MOTOR_STOP, 'UTF-8')
+        )
+        self.serial.close()
 
 
 if __name__ == "__main__":
 
     commander = SerialCommander()
-    commander.listen()
-    rospy.spin()
+    
+    try:    
+        commander.listen()
+        rospy.spin()
+
+    finally:
+        commander.clean()
