dce533f0bdd32d4ed2e6b4cf03651b9d580e32b5
Peng <peng.wei@murobotix.com>
2022-08-10 15:59:07 -0700

    add serial reading

--- src/voc-sampler/scripts/sampler_commander.py
+++ src/voc-sampler/scripts/sampler_commander.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from tkinter import Variable
 import serial
 import rospy
 
@@ -8,28 +9,37 @@ from voc_sampler.srv import CommandSampler
 class SerialCommander(object):
     def __init__(self):
         rospy.init_node("voc_sampler")
+        self.rate = rospy.Rate(30)
         rospy.loginfo("[voc_sampler]: Node Started!")
 
-        # Get ROS parameters
+        self.init_variable()
+        self.get_ros_parameter()
+        self.define_service()
+
+    def init_variable(self):
+        self.is_active = False
+
+    def get_ros_parameter(self):
         port = rospy.get_param("~port", "/dev/ttyACM0")
         baud_rate = rospy.get_param("~baud_rate", 115200)
-        self.serial = serial.Serial(port, baud_rate)    
+        self.serial = serial.Serial(port, baud_rate)
 
-        # ROS Service
+    def define_service(self):
         rospy.Service("command_sampler", CommandSampler, self.command_sampler)
-        
-        # ROS Service Proxy
         self.send_command_to_sampler = rospy.ServiceProxy("command_sampler", CommandSampler)
 
     def command_sampler(self, req):
         # s: Recording Start
         # r: Recording Stop
-        self.serial.write(bytes(req.command, 'UTF-8'))
        
         if req.command == CommandSampler._request_class.COMMAND_MOTOR_START:
+            self.serial.write(bytes(req.command, 'UTF-8'))
+            self.is_active = True       
             rospy.loginfo("[voc_sampler]: Motor Started!")
 
         elif req.command == CommandSampler._request_class.COMMAND_MOTOR_STOP:
+            self.serial.write(bytes(req.command, 'UTF-8'))
+            self.is_active = False
             rospy.loginfo("[voc_sampler]: Motor Stoped!")
         
         else:
@@ -37,8 +47,17 @@ class SerialCommander(object):
         
         return True
 
+    def listen(self):
+        while not rospy.is_shutdown():
+            if self.is_active:
+                msg = self.serial.readline().decode("utf-8")
+                print(msg)
+            
+            self.rate.sleep()
+
 
 if __name__ == "__main__":
 
     commander = SerialCommander()
+    commander.listen()
     rospy.spin()
